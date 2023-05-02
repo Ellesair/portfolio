52a96511c9844f623d113c29a40ac0839a35238e
K Lake <klake.coding@gmail.com>
2023-05-02 15:20:10 -0500

    App(contact): Added submit button, removed break tags, added label tags for readability/accessibility

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function contact() {
   return (
     <>
-    <h2>Send me something!</h2>
-    <form>
-      <input type="text" placeholder="Title"></input>
-      <br />
-      <input type="text" placeholder="Your Name"></input>
-      <br />
-      <textarea></textarea>
-    </form>
+      <h2>Send me something!</h2>
+      <form>
+        <label>
+          <input type="text" placeholder="Title"></input>
+        </label>
+        <label>
+          <input type="text" placeholder="Your Name"></input>
+        </label>
+        <label>
+          <textarea></textarea>
+        </label>
+        <button type="submit">SUBMIT</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default contact
+export default contact;
