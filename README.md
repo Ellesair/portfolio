0fb777d30f1e8caf4a870b584952cc4a307572de
K Lake <klake.coding@gmail.com>
2023-05-03 00:30:07 -0500

    App(Footer): Inserted social media svg links with styles.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 The purpose of this next.js project is to not only showcase my personal projects
 but to also display my current knowledge (as of making this) in regards to clean 
-code practices as well as my knowledge of accessibility and UI/UX design.
+code practices as well as my knowledge of accessible code while displaying my personal style.
+
+This site was built with the mobile first approach so it is responsive on multiple screen sizes.
+
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function footer() {
   return (
-    <div>insert link list with social media icon links</div>
-  )
+    <>
+      <section className={styles.socialLinks}>
+        <Link href="https://github.com/Ellesair" target="_blank">
+          <FontAwesomeIcon className={styles.socialLink} icon={faGithub} />
+        </Link>
+        <Link href="https://www.linkedin.com/in/krystallake/" target="_blank">
+        <FontAwesomeIcon className={styles.socialLink} icon={faLinkedinIn} />
+        </Link>
+      </section>
+    </>
+  );
 }
 
-export default footer
+export default footer;
